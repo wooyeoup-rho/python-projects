ef42c73ff4de9e4c6da9e797d0845f7bd5733b3b
Wooyeoup Rho <wooyeoup.rho@gmail.com>
2025-02-01 18:56:30 -0400

    flask/capstone-blog: Add POST requests for contact page.

--- flask/capstone-blog/main.py
+++ flask/capstone-blog/main.py
@@ -1,6 +1,14 @@
-from datetime import datetime
-from flask import Flask, render_template
-import requests
+from dotenv import load_dotenv, dotenv_values
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from flask import Flask, render_template, request
+import requests, smtplib
+
+load_dotenv()
+config = dotenv_values(".env")
+
+EMAIL = config.get("SENDING_EMAIL")
+EMAIL_PASSWORD = config.get("SENDING_EMAIL_PWD")
 
 app = Flask(__name__)
 
@@ -10,22 +18,52 @@ blog_posts = response.json()
 
 @app.route("/")
 def home():
-    current_year = datetime.now().year
-    return render_template("index.html", header_image="home-bg.jpg", heading="Clean Blog", subheading="A Blog Theme by Start Bootstrap", blog_posts=blog_posts, year=current_year)
+    return render_template("index.html", blog_posts=blog_posts)
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html", header_image="contact-bg.jpg", heading="Contact Me", subheading="Have questions? I have answers.")
+    if request.method == "POST":
+        msg = MIMEMultipart()
+        msg["From"] = EMAIL
+        msg["To"] = EMAIL
+        msg["Subject"] = "New message!"
+
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+
+        html_content = f"""
+        <html>
+        <body>
+            <p><b>Name:</b> {name}</p>
+            <p><b>Email:</b> {email}</p>
+            <p><b>Phone:</b> {phone}</p>
+            <p><b>Message:</b><br/>{message}</p>
+        </body>
+        </html>
+        """
+
+        msg.attach(MIMEText(html_content, "html"))
+
+        with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=EMAIL, password=EMAIL_PASSWORD)
+            connection.sendmail(msg["From"], msg["To"], msg.as_string())
+
+        return render_template("contact.html", heading="Message Sent")
+    else:
+        return render_template("contact.html", heading="Contact Me")
 
 @app.route("/about")
 def about():
-    return render_template("about.html", header_image="about-bg.jpg", heading="About Me", subheading="This is what I do.")
+    return render_template("about.html")
 
 @app.route("/post/<int:blog_id>")
 def blog_post(blog_id):
     post = blog_posts[blog_id - 1]
 
-    return render_template("post.html", post=post["body"], header_image=post["image_url"], heading=post["title"], subheading=post["subtitle"])
+    return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ flask/capstone-blog/tutorial/html-forms/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def receive_data():
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+        return f"<h1>Username: {name}, Password: {password}</h1>"
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
